daf77bdec475f6c1ff0b45009ec8911f9e5079bd
Altynaika <altynaika.toktomuratovaa@gmail.com>
2021-02-21 18:18:17 +0600

    first commit

---
+++ app/src/main/java/com/example/exampleapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.exampleapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            /* Create an Intent that will start the MainActivity. */
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 800)
+    }
+}
